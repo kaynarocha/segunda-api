4cf9ee79c627a42e38b57c2b0d685515083c5e50
KAYNA DA SILVA ROCHA <kayna.rocha@alunos.sc.senac.br>
2026-02-26 20:17:06 -0300

    add hello world controller

---
+++ src/main/java/br/com/senac/segunda_api/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package br.com.senac.segunda_api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/helloWorld")
+public class HelloWorldController {
+
+    @GetMapping("/teste")
+    public ResponseEntity<String> teste() {
+        return ResponseEntity.ok("Olá Mundo!");
+    }
+}
