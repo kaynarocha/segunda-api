bf6be667ac5cc7811d0fbdc9b3c104217ae5056e
KAYNA DA SILVA ROCHA <kayna.rocha@alunos.sc.senac.br>
2026-02-26 19:54:46 -0300

    primeiro commit

---
+++ src/main/java/br/com/senac/segunda_api/SegundaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.segunda_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaApiApplication.class, args);
+	}
+
+}
